a7cefa99394a3fb57a1ef3b71db3da310434b9c6
xiongguoquan <17876872031@163.com>
2025-09-06 15:41:27 +0800

    新的修改

---
+++ demo/src/main/java/com/example/demo/control/Main.java
@@ -0,0 +1,8 @@
+package com.example.demo.control;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("我是大帅逼");
+        System.out.println("林凤娇是大傻逼");
+    }
+}
